498281ee9d11022c0ca28ddf95894570de718473
Peter H <wappospypuppy@gmail.com>
2020-09-12 16:55:54 -0700

    Started outlining some server code and wrote some test console client code just for testing.

--- GMRTSConsoleClient/Program.cs
+++ GMRTSConsoleClient/Program.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using GMRTSClasses;
+
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Numerics;
 using System.Text;
@@ -9,14 +12,23 @@ namespace GMRTSConsoleClient
 {
     class Program
     {
-        static event Action a = null;
-        static void Main(string[] args)
+        static double identity(int n)
         {
-            a += Program_a;
-            a?.Invoke();
+            return n;
+        }
 
-            Guid guid = Guid.NewGuid();
+
+        static async Task Main(string[] args)
+        {
+
+            SignalRClient client = new SignalRClient("http://localhost:61337/", "GameHub", a => null, TimeSpan.FromMilliseconds(400));
+            bool success = await client.TryStart();
             ;
+            ;
+            Console.ReadLine();
+            await client.MoveAction(new GMRTSClasses.CTSTransferData.MoveAction() { Positions = new List<Vector2>() { new Vector2(100, 200) }, UnitIDs = new List<Guid> { Guid.NewGuid() } });
+            await client.MoveAction(new GMRTSClasses.CTSTransferData.MoveAction() { Positions = new List<Vector2>() { new Vector2(200, 100) }, UnitIDs = new List<Guid> { Guid.NewGuid() } });
+            Console.ReadLine();
         }
 
         private static void Program_a()
