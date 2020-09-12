4b95e2b399f8f740205fcea81dfa15837990d582
Peter H <wappospypuppy@gmail.com>
2020-09-12 00:17:00 -0700

    Made hub class but didn't do anything with it. Just put down a few lines to get this branch started.

---
+++ GMRTSServer/GameHub.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNet.SignalR;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GMRTSServer
+{
+    public class GameHub : Hub
+    {
+
+    }
+}
--- GMRTSServer/Program.cs
+++ GMRTSServer/Program.cs
@@ -1,8 +1,15 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
+using Microsoft.AspNet.SignalR.Hubs;
+using Microsoft.Owin.Cors;
+using Microsoft.Owin.Hosting;
+
+using Owin;
 
 namespace GMRTSServer
 {
@@ -10,6 +17,20 @@ namespace GMRTSServer
     {
         static void Main(string[] args)
         {
+            string url = "http://localhost:61337/";
+            using (WebApp.Start(url))
+            {
+                Console.WriteLine("Server running on {0}", url);
+                Console.ReadLine();
+            }
+        }
+    }
+    class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseCors(CorsOptions.AllowAll);
+            app.MapSignalR();
         }
     }
 }
