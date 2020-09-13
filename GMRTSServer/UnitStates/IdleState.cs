87ac334f3e047cfa497b12fbe9ec3a33e56f1755
Peter H <wappospypuppy@gmail.com>
2020-09-12 19:59:00 -0700

    Wrote some more code. Whee!
    Made Update function and outlined some more stuff.

---
+++ GMRTSServer/UnitStates/IdleState.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GMRTSServer.UnitStates
+{
+    internal class IdleState : IUnitState
+    {
+        public IUnitState Update(ulong currentMilliseconds, float elapsedTime)
+        {
+            return this;
+        }
+    }
+}
