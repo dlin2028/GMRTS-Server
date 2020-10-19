357292bfd542657b61a2dc34cdc9dcf7dd32b069
Peter H <wappospypuppy@gmail.com>
2020-10-18 19:16:40 -0700

    Outlined new class.

---
+++ GMRTSServer/FlowfieldMovementCalculator.cs
@@ -0,0 +1,19 @@
+﻿using GMRTSServer.ServersideUnits;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GMRTSServer
+{
+    class FlowfieldMovementCalculator : IMovementCalculator
+    {
+        public Vector2 ComputeVelocity(Game game, Unit unit, Vector2 destination)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
