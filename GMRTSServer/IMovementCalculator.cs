23f504cc9746d70e2993ba927201a788f02469e0
Peter H <wappospypuppy@gmail.com>
2020-10-18 19:13:01 -0700

    Made basic IMovementCalculator interface.

---
+++ GMRTSServer/IMovementCalculator.cs
@@ -0,0 +1,16 @@
+﻿using GMRTSServer.ServersideUnits;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GMRTSServer
+{
+    interface IMovementCalculator
+    {
+        Vector2 ComputeVelocity(Game game, Unit unit, Vector2 destination);
+    }
+}
